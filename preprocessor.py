b1d8d1e0591418405e0d6bf5aac518b6d3c8bf6a
Ashwin_Naik <ashwincnaik18@gmail.com>
2026-08-13 22:10:17 +0530

    Initial Commit

--- preprocessor.py
+++ preprocessor.py
@@ -33,39 +33,74 @@ def format_dataset(conversation):
 
 def split_long_conversations(tokens):
     chunks = []
-    
-    for i in range(0, len(tokens), BLOCK_SIZE):
+    t = len(tokens)
+    r = t // BLOCK_SIZE
+    for i in range(0, r * 512, BLOCK_SIZE):
         chunk = tokens[i: i + BLOCK_SIZE]
         chunks.append(chunk)
         
-    return chunks
+    leftover = tokens[t-(t % BLOCK_SIZE):] # or simply (block_size * r + 1)
+    return chunks, leftover
 
+
+# def pack_conversations(conversations):
+#     chunk = []
+#     current_chunk = []
+    
+#     for conversation in conversations:
+#         tokens = format_dataset(conversation)
+        
+#         if len(tokens) > BLOCK_SIZE:
+            
+#             if current_chunk:
+#                 chunk.append(current_chunk)
+#                 current_chunk = []
+                
+                
+#             completed_chunk, leftover = split_long_conversations(tokens)
+            
+#             chunk.extend(completed_chunk)
+            
+#             if (len(current_chunk) + len(leftover)) <= BLOCK_SIZE:
+#                 current_chunk.extend(leftover)
+                
+#             else:
+#                 chunk.append(current_chunk)
+#                 current_chunk = leftover
+            
+#             continue
+        
+        
+#         if len(current_chunk) + len(tokens) <= BLOCK_SIZE:
+#             current_chunk.extend(tokens)
+            
+            
+#         else:
+#             chunk.append(current_chunk)
+#             current_chunk = tokens.copy()
+            
+#     if current_chunk:
+#         chunk.append(current_chunk)
+        
+#     return chunk
 
 def pack_conversations(conversations):
-    chunk = []
-    current_chunk = []
+    MIN_CHUNK_SIZE = 512
     
+    buffer = []
+
     for conversation in conversations:
+
         tokens = format_dataset(conversation)
-        
-        if len(tokens) >= BLOCK_SIZE:
-            if current_chunk:
-                chunk.append(current_chunk)
-                
-            c = split_long_conversations(tokens)
-            chunk.extend(c)
+
+        if not tokens:
             continue
-        
-        
-        if len(current_chunk) + len(tokens) <= BLOCK_SIZE:
-            current_chunk.extend(tokens)
-            
-            
-        else:
-            chunk.append(current_chunk)
-            current_chunk = tokens.copy()
-            
-    if current_chunk:
-        chunk.append(current_chunk)
-        
-    return chunk
+
+        buffer.extend(tokens)
+
+        while len(buffer) >= BLOCK_SIZE:
+            yield buffer[:BLOCK_SIZE]
+            buffer = buffer[BLOCK_SIZE:]
+
+    if len(buffer) >= MIN_CHUNK_SIZE:
+        yield buffer
